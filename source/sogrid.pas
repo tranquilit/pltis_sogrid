5db9a2cf1befe92511acb8e835ac215e0205f50d
Marcos Douglas B. Santos <msantos@tranquil.it>
2023-11-22 19:27:24 -0300

    grid: if has filters, it should set ShowAutoFilters automatically

--- source/sogrid.pas
+++ source/sogrid.pas
@@ -968,7 +968,10 @@ begin
           end;
           // add an MARK_ARROW in header column text, if there are filters for this column
           if Pos(MARK_ARROW, vColumn.Text) = 0 then
+          begin
             vColumn.Text := vColumn.Text + MARK_ARROW;
+            fShowAutoFilters := True;
+          end;
         end;
       end
       else
