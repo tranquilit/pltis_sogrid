115022b6926f44adbacfb6e135ede4bcf72e8ca8
htouvet <htouvet@tranquil.it>
2025-10-13 16:36:01 +0200

    [FIX] header height highdpi scaling

--- source/sogrid.pas
+++ source/sogrid.pas
@@ -2707,10 +2707,10 @@ var
 begin
   if (AMode in [lapAutoAdjustForDPI]) then
   begin
-    Header.MinHeight := max(18,round(Header.MinHeight * AXProportion)+1);
-    Header.MaxHeight := max(18,round(Header.MaxHeight * AXProportion)+1);
-    Header.DefaultHeight := max(18,round(Header.DefaultHeight * AXProportion)+1);
-    Header.Height := max(18,round(Header.Height * AXProportion)+1);
+    Header.MinHeight := max(18,round(Header.MinHeight * AYProportion)+1);
+    Header.MaxHeight := max(18,round(Header.MaxHeight * AYProportion)+1);
+    Header.DefaultHeight := max(18,round(Header.DefaultHeight * AYProportion)+1);
+    Header.Height := max(18,round(Header.Height * AYProportion)+1);
 
     for i := 0 to header.Columns.Count-1 do
     begin
